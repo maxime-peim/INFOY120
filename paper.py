4431da0a8c5cd9bd3ee003f8d1ae60896ce9302b
Maxime Peim <maxime.peim@gmail.com>
2022-03-07 00:07:41 +0100

    First working classification

--- paper.py
+++ paper.py
@@ -1,17 +1,22 @@
-import dataset
+from numpy import mean, std
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+
+import dataset as ds
+import feature as ft
 
 def build_paper_datasets():
     # building human dataset
-    HUM = dataset.Dataset("datasets/human/E13",
-                          "datasets/human/TFP")
+    HUM = ds.Dataset("datasets/human/E13",
+                     "datasets/human/TFP")
     HUM.name = "HUM"
 
     # building fake dataset
-    FAK = dataset.Dataset("datasets/fake/FSF",
-                          "datasets/fake/INT",
-                          "datasets/fake/TWT")
+    FAK = ds.Dataset("datasets/fake/FSF",
+                     "datasets/fake/INT",
+                     "datasets/fake/TWT")
     FAK.name = "FAK"
-    FAK.inplace_undersample(HUM.size)
+    FAK.undersample(HUM.size)
 
     # building base dataset
     BAS = HUM + FAK
@@ -22,5 +27,11 @@ def build_paper_datasets():
 
 if __name__ == "__main__":
     HUM, FAK, BAS = build_paper_datasets()
-
-    print(HUM, FAK, BAS)
+    
+    X, y = BAS.make_classification(ft.class_A)
+    
+    # https://machinelearningmastery.com/random-forest-ensemble-in-python/
+    RF_model = RandomForestClassifier()
+    n_scores = cross_val_score(RF_model, X, y, scoring='accuracy', cv=10, n_jobs=-1, error_score='raise')
+    
+    print('MAE: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
