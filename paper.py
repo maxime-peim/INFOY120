39f7238da118b1f9be317ad0b1033b020606c8dd
Maxime Peim <maxime.peim@gmail.com>
2022-03-12 23:48:12 +0100

    paper.py to save results

--- paper.py
+++ paper.py
@@ -1,59 +1,28 @@
 import logging
 import sys
 
-from numpy import mean
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (
-    f1_score,
-    make_scorer,
-    matthews_corrcoef,
-    precision_score,
-    recall_score,
-)
-from sklearn.model_selection import cross_validate
+import utils
+import feature as ft
 
-from dataset import Dataset
-from feature import class_A, class_B, class_C
-
-
-def build_paper_datasets():
-    # building human dataset
-    HUM = Dataset("datasets/human/E13", "datasets/human/TFP")
-    HUM.name = "HUM"
-
-    # building fake dataset
-    FAK = Dataset("datasets/fake/FSF", "datasets/fake/INT", "datasets/fake/TWT")
-    FAK.name = "FAK"
-    FAK.undersample(HUM.size)
-
-    # building base dataset
-    BAS = HUM + FAK
-    BAS.name = "BAS"
-
-    return HUM, FAK, BAS
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout)
 
-    HUM, FAK, BAS = build_paper_datasets()
+    HUM, FAK, BAS = utils.build_paper_datasets()
+    features_evaluation, labels = BAS.evaluate_features(ft.all_features)
 
-    X, y = BAS.make_classification(class_A | class_B | class_C)
+    evaluations = [
+        (ft.class_A, "results/paper/class_A_alone.txt"),
+        (ft.class_B, "results/paper/class_B_alone.txt"),
+        (ft.class_C, "results/paper/class_C_alone.txt"),
+        (ft.class_A | ft.class_B, "results/paper/class_AB.txt"),
+        (ft.all_features, "results/paper/all_features.txt"),
+        (ft.set_Y, "results/paper/set_yang.txt"),
+        (ft.set_S, "results/paper/set_stringhini.txt"),
+    ]
 
-    # https://machinelearningmastery.com/random-forest-ensemble-in-python/
-    RF_model = RandomForestClassifier()
-    scoring = {
-        "Accuracy": "accuracy",
-        "Precision": make_scorer(precision_score, pos_label="human"),
-        "Recall": make_scorer(recall_score, pos_label="human"),
-        "F-M": make_scorer(f1_score, pos_label="human"),
-        "MCC": make_scorer(matthews_corrcoef),
-        "AUC": "roc_auc",
-    }
-    scores = cross_validate(
-        RF_model, X, y, scoring=scoring, cv=10, n_jobs=-1, error_score="raise"
-    )
-
-    for score_name, values in scores.items():
-        if score_name.startswith("test_"):
-            print(f"{score_name[5:]}: {mean(values):.4f}")
+    for features, path in evaluations:
+        logger.info(f"Evaluating {features.name} over all datasets.")
+        utils.classify_and_save(features_evaluation[features.names], labels, path)
